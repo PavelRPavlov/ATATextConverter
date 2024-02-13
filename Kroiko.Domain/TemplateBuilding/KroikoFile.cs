fdbd1d4e88267a708393ca67ccef21ef01ff161f
Pavel <pavel.pavlov@zuhlke.com>
2024-02-13 15:19:36 +0200

    add option to create doubled details and custom operations on edges

--- Kroiko.Domain/TemplateBuilding/KroikoFile.cs
+++ Kroiko.Domain/TemplateBuilding/KroikoFile.cs
@@ -1,4 +1,6 @@
-﻿namespace Kroiko.Domain.TemplateBuilding;
+﻿using Kroiko.Domain.CellsExtracting;
+
+namespace Kroiko.Domain.TemplateBuilding;
 public class LoniraDetail : IKroikoDetail
 {
     public string? LoniraEdges { get; set; }
@@ -18,10 +20,10 @@ public class SuliverDetail : IKroikoDetail
     public string? Cabinet { get; set; }
     public double MaterialThickness { get; set; }
     public byte IsGrainDirectionReversed { get; set; }
-    public byte LongEdge { get; set; }
-    public byte LongEdge2 { get; set; }
-    public byte ShortEdge { get; set; }
-    public byte ShortEdge2 { get; set; }
+    public string? LongEdge { get; set; }
+    public string? LongEdge2 { get; set; }
+    public string? ShortEdge { get; set; }
+    public string? ShortEdge2 { get; set; }
     public string? Note { get; set; }
 }
 
